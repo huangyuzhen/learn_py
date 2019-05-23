2d9f42f5df354f4768ef01e4252318a386f03fa9
huangyuzhen <cacaty@qq.com>
2019-05-23 17:25:39 +0800

    10

---
+++ c10.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+
+
+def stringCount(aString):
+    result = ''
+
+    lastChar = ''
+    lastCount = 0
+    for s in aString:
+        if s == lastChar:
+            lastCount += 1
+        if s != lastChar:
+            if lastChar != '':
+                result += '{:d}{:s}'.format(lastCount, lastChar)
+
+            lastCount = 1
+            lastChar = s
+
+    if lastChar != '':
+        result += '{:d}{}'.format(lastCount, lastChar)
+
+    return result
+
+
+def stringCount2(aString):
+    result = ''
+    match = re.finditer(r'(\d)\1*', aString)
+    for m in match:
+        s = m.group()
+        result += '{:d}{}'.format(len(s), s[0])
+    return result
+
+
+def stringCount3(aString):
+    def repl(m):
+        s = m.group()
+        return '{:d}{}'.format(len(s), s[0])
+
+    return re.sub(r'(\d)\1*', repl, aString)
+
+
+s = '1'
+for i in range(30):
+    s = stringCount3(s)
+    # print(s)
+
+print(len(s))
