52b5bc4ea6967f901e1da28710f70a11a0a90c64
huangyuzhen <cacaty@qq.com>
2019-05-24 19:48:00 +0800

    update 13

--- c13.py
+++ c13.py
@@ -1,13 +1,18 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import xmlrpclib
+# #!/usr/bin/env python2
+
+# import xmlrpclib
+
+
+from xmlrpc import client as xmlrpclib
 
 xmlrpc = xmlrpclib.ServerProxy("http://www.pythonchallenge.com/pc/phonebook.php")
 evil = 'Bert'
 
-print xmlrpc.system.listMethods()
+print(xmlrpc.system.listMethods())
 # ['phone', 'system.listMethods', 'system.methodHelp', 'system.methodSignature', 'system.multicall', 'system.getCapabilities']
 
-print xmlrpc.system.methodHelp('phone')
-print xmlrpc.phone(evil)
+print(xmlrpc.system.methodHelp('phone'))
+print(xmlrpc.phone(evil))
